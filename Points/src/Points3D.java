964deff3408d6678b341291b932caf894b24ced1
Dell <Dell@DESKTOP-4RLDQ3D>
2018-08-20 10:22:51 +0200

    ProjectEuler Problem6

--- Points/src/Points3D.java
+++ Points/src/Points3D.java
@@ -10,7 +10,10 @@ public class Points3D extends Points2D{
 		super();
 		this.z = z;
 	}
+	@Override
 	public void print() {
+		System.out.println("X = " + x);
+		System.out.println("Y = " + y);
 		System.out.println("Z = " + z);
 	}
 }
