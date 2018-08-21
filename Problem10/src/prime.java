88569a07ffdf9c5279b22ff21761d1e228215c0a
Lithaos <wojto3@op.pl>
2018-08-21 12:22:55 +0200

    ProjectEuler Problem10

---
+++ Problem10/src/prime.java
@@ -0,0 +1,28 @@
+
+public class prime {
+	
+	boolean czy_pierwsza(int n)
+	{
+		if(n<2)
+			return false; 
+			
+		for(int i=2;i*i<=n;i++)
+			if(n%i==0)
+				return false; 
+		return true;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prime test = new prime();
+		long sum=0;
+		for(int i=2;i<2000000;i++)
+		{
+			if(test.czy_pierwsza(i))
+			{
+				sum+=i;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
