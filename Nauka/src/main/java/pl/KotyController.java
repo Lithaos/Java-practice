7c7e80902d63617a0b65896f88a80d0af8109831
Lithaos <wojto3@op.pl>
2018-08-29 18:36:12 +0200

    Testing new features

--- Nauka/src/main/java/pl/KotyController.java
+++ Nauka/src/main/java/pl/KotyController.java
@@ -2,15 +2,23 @@ package pl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class KotyController {
 	@Autowired
 	private KotDAO kotDao;
 
-	@RequestMapping("/metoda")
-	public String metoda() {
+	@RequestMapping(value = "/" , method = RequestMethod.GET)
+	public String home() {
 		return "glowny";
 	}
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+		public String welcome(@RequestParam String nazwisko, ModelMap model) {
+		model.put("nazwisko", nazwisko);
+			return "welcome";
+	}	
 }
--- Nauka/src/main/webapp/WEB-INF/views/glowny.jsp
+++ Nauka/src/main/webapp/WEB-INF/views/glowny.jsp
@@ -2,15 +2,20 @@
     pageEncoding="UTF-8"%>
 <html>
 <head>
-<title>Tytuł</title>
+<title>Projekt</title>
 </head>
 <body>
-	<h1>Tytuł Akapitu</h1>
-	<p>Akapit tekstu</p>
-	<a href="http://kobietydokodu.pl">Wejdź na naszą stonę </a>
-	<ul>
-		<li>Przykład</li>
-		<li>Inny Przykład</li>
-	</ul>
+	<h1>Fomularz dodawania rodziny.</h1>
+	<p>Wprowadz dane do formularza</p>
+	<form action = "/" method="post">
+		Podaj nazwisko <input type="text" name="nazwisko" value="nazwisko">  <br>
+		
+		Podaj Imię <input type="text" name="imie" value="imie"> <br>
+		
+		Podaj Pesel <input type="number" name="pesel" value="numer"> <br>
+		
+		Podaj Adres <input type="text" name="adres" value="Adres"> <br>
+		 <input type="submit" value="Wyślij">
+	</form>
 </body>
 </html>
