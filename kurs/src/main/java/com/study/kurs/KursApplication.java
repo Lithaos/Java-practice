cd24ec065fbb2c54198bc676c7843589d36b4e73
Lithaos <wojto3@op.pl>
2018-09-03 18:44:29 +0200

    Course Start

---
+++ kurs/src/main/java/com/study/kurs/KursApplication.java
@@ -0,0 +1,12 @@
+package com.study.kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KursApplication.class, args);
+	}
+}
---
+++ kurs/src/main/java/com/study/kurs/Starter.java
@@ -0,0 +1,24 @@
+package com.study.kurs;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import domain.Knight;
+import domain.Quest;
+
+@Component
+public class Starter implements CommandLineRunner {
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Quest saveThePrincess = new Quest("Uratuj ksiezniczke");
+		Knight lancelot = new Knight ("Lancelot", 29 , saveThePrincess);
+		System.out.println(lancelot);
+	
+		Quest killTheDragon = new Quest("Zabij smoka!");
+		Knight percival = new Knight("Percival", 31, killTheDragon);
+		System.out.println(percival);
+
+	}
+}
